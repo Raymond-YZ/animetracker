ca57c1b7abb15d7124aae15024ad824efbfff17f
Raymond Lee <74083400+Raymond-YZ@users.noreply.github.com>
2021-05-29 00:24:46 -0700

    Add files via upload

---
+++ private/secret_settings.py
@@ -0,0 +1,8 @@
+# single sign on Google (will be used if provided)
+OAUTH2GOOGLE_CLIENT_ID = "160899355244-q7qsnnmemuf3p7jj9js1db1rhdp2eqbb.apps.googleusercontent.com"
+OAUTH2GOOGLE_CLIENT_SECRET = "GHS3HN5pcD_J4QteB6bEoR1e"
+
+DB_USER="appuser"
+DB_NAME="user_list"
+DB_PASSWORD="castillion114"
+DB_CONNECTION="anime-tracker-314708:us-west2:anime-tracker-183-1"
